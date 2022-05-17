3b5976e89ff8517e7a593aebe356424048f82423
Joemon-Jesudas <85217855+Joemon-Jesudas@users.noreply.github.com>
2022-05-17 16:00:00 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,8 @@ curr_path = os.path.dirname(os.path.realpath(__file__))
 rf = joblib.load(curr_path + '/model.joblib')
 
 
-def predict(attributes: np.array):   
+def predict(attributes: np.array):
+    
     pred = rf.predict(attributes)
     print("Credit score predictor")
     return pred[0]
